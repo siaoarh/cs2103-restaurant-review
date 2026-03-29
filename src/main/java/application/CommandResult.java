4831e088d66f26966f9d869168cdddc161b019d1
siaoarh <elluimelliot@gmail.com>
2026-03-29 18:00:57 +0800

    Checkstyle modifications

--- src/main/java/application/CommandResult.java
+++ src/main/java/application/CommandResult.java
@@ -1,7 +1,9 @@
 package application;
 
 /**
- * Represents the result of handling a single user command. Used in MealMeter to encapsulate both the output message and whether the application should terminate after this command.
+ * Represents the result of handling a single user command.
+ * Used in MealMeter to encapsulate both the output message and whether the application
+ * should terminate after this command.
  *
  * @param output the output message to display to the user
  * @param shouldTerminate whether the application should terminate after this command
--- src/main/java/application/storage/StorageLoadResult.java
+++ src/main/java/application/storage/StorageLoadResult.java
@@ -5,7 +5,9 @@ import java.util.List;
 import application.review.ReviewList;
 
 /**
- * Represents the result of loading reviews from storage. Used in MealMeter to encapsulate both the loaded reviews and any warnings that occurred during loading.
+ * Represents the result of loading reviews from storage.
+ * Used in MealMeter to encapsulate both the loaded reviews and any
+ * warnings that occurred during loading.
  *
  * @param reviewList the reviews loaded from storage
  * @param warnings non-fatal warnings encountered while loading
